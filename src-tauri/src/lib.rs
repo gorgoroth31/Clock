5301a18a2d2b5fe3d6e406b6949d33e03c5583c1
gorgoroth31 <it.glen@mailbox.org>
2026-01-29 23:11:28 +0100

    refactored name & file structure

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,8 +2,6 @@ use std::env;
 use std::fs::{self, File};
 use std::io::prelude::*;
 use std::path::Path;
-use std::fs::OpenOptions;
-use std::io::Write;
 use dirs;
 use dotenv::dotenv;
 
@@ -30,23 +28,21 @@ fn is_dark_mode() -> bool {
 
 #[tauri::command]
 fn read_times() -> Vec<String> {
-    let whole_path_string = get_file_path();
+    let whole_path_string = get_alarms_path();
 
-    let path = Path::new(&whole_path_string);
-    let display = path.display();
+    let paths = fs::read_dir(&whole_path_string).unwrap();
 
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
+    let mut v: Vec<String> = Vec::new();
 
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => println!("{} has been read", display),
+    for path in paths {
+        let time = path.unwrap().file_name().display().to_string();
+        let (hours, minutes) = time.split_at(2);
+        let mut time_as_string = hours.to_owned();
+        time_as_string.push_str(":");
+        time_as_string.push_str(minutes);
+        v.push(time_as_string);
     }
 
-    let mut v: Vec<String> = serde_json::from_str(&s).unwrap();
     v.sort();
     v
 }
@@ -55,66 +51,55 @@ fn read_times() -> Vec<String> {
 fn add_time(time: String) {
     println!("time will be added: {}", time);
 
-    let mut data = read_times();
+    let alarms_path_string = get_path_for_time(time);
+    
+    let path_exists = Path::new(&alarms_path_string).exists();
 
-    data.push(time);
-
-    let serialized = serde_json::to_string(&data).unwrap();
-
-    let whole_path_string = get_file_path();
-
-    let mut file = OpenOptions::new()
-        .write(true)
-        .open(whole_path_string)
-        .expect("cannot open file");
-
-    file.write(serialized.as_bytes()).expect("adding time failes");
+    if !path_exists {
+        println!("creating file {}", alarms_path_string);
+        File::create_new(&alarms_path_string).expect("creation failed");
+    }
 }
 
 #[tauri::command]
 fn delete_time(time: String) {
     println!("time will be deleted: {}", time);
 
-    let mut data = read_times();
+    let file_path = get_path_for_time(time);
 
-    data.retain(|v| v != &time);
-
-    let serialized = serde_json::to_string(&data).unwrap();
-
-    let whole_path_string = get_file_path();
-
-    let mut file = OpenOptions::new()
-        .write(true)
-        .open(whole_path_string)
-        .expect("cannot open file");
-
-    // needed to remove contents of file before writing new data
-    // no idea why, but without this line, the data is not being written properly
-    file.set_len(0).unwrap();
-
-    file.write(serialized.as_bytes()).expect("deleting time failes");
+    fs::remove_file(file_path).unwrap();
 }
 
-fn get_file_path() -> String {
-    let filename = env::var("FILE_NAME").unwrap();
+fn get_path_for_time(time: String) -> String {
+    let sanitized_time = time.replace(":", "");
+    let mut alarms_path_string = get_alarms_path();
+    alarms_path_string.push_str("/");
+    alarms_path_string.push_str(&sanitized_time);   
+    alarms_path_string
+}
+
+fn get_alarms_path() -> String {
+    let alarms = env::var("ALARMS").unwrap();
     let directory = env::var("DIRECTORY").unwrap();
     let home_path = dirs::home_dir().unwrap().display().to_string();
     
     let mut whole_path_string : String = home_path.to_owned();
     whole_path_string.push_str(&directory);
-    whole_path_string.push_str("/");
-    whole_path_string.push_str(&filename);
+    whole_path_string.push_str(&alarms);
     whole_path_string
 }
 
-fn create_file_if_not_exists() {
-    let filename = env::var("FILE_NAME").unwrap();
+fn create_directories_if_not_existing() {
+    let alarms = env::var("ALARMS").unwrap();
     let directory = env::var("DIRECTORY").unwrap();
     let home_path = dirs::home_dir().unwrap().display().to_string();
 
-    let mut whole_path_string : String = home_path.to_owned();
-    whole_path_string.push_str(&directory);
-    
+    let mut alarms_path_string : String = home_path.to_owned();
+    alarms_path_string.push_str(&directory);
+    alarms_path_string.push_str(&alarms);
+
+    fs::create_dir_all(alarms_path_string).unwrap();
+    /* 
     let directory_path = Path::new(&whole_path_string);
 
     let directory_exists = directory_path.exists();
@@ -135,13 +120,14 @@ fn create_file_if_not_exists() {
 
         file.write("[]".as_bytes()).expect("write failed");
     }
+    */
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     dotenv().ok();
 
-    create_file_if_not_exists();
+    create_directories_if_not_existing();
 
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    cosmic_clock_lib::run()
+    clock_lib::run()
 }
