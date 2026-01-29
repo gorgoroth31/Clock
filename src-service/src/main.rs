e823b28adb932033e40ca971321f5b45dd3723d1
gorgoroth31 <it.glen@mailbox.org>
2026-01-30 00:50:35 +0100

    added file play support

--- src-service/src/main.rs
+++ src-service/src/main.rs
@@ -2,6 +2,8 @@ use dotenv::dotenv;
 use chrono::prelude::*;
 use std::{thread, time};
 use std::time::{SystemTime};
+use std::fs::File;
+use std::io::BufReader;
 use src_helper;
 
 fn main() {
@@ -17,8 +19,15 @@ fn main() {
         let hour = datetime.hour();
         let minute = datetime.minute();
 
-        for alarm in all_alarms {
+        for _alarm in all_alarms {
             println!("{}:{}", hour, minute);
+            let stream_handle = rodio::OutputStreamBuilder::open_default_stream()
+                    .expect("open default audio stream");
+
+            let file = BufReader::new(File::open("alarm.mp3").unwrap());
+            let _sink = rodio::play(&stream_handle.mixer(), file).unwrap();
+
+            std::thread::sleep(std::time::Duration::from_secs(5));
         }
 
         let current_seconds = Utc::now().second();
